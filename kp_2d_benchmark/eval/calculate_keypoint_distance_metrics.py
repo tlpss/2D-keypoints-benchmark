28ff141f8c97c3631dc0132d226ef0f28c772fed
tlpss <thomas17.lips@gmail.com>
2025-01-07 15:38:34 +0100

    start on eval

---
+++ kp_2d_benchmark/eval/calculate_keypoint_distance_metrics.py
@@ -0,0 +1,111 @@
+from kp_2d_benchmark.eval.coco_results import COCOKeypointResults, CocoKeypointsDataset
+
+def calculate_keypoint_distances(coco_dataset: CocoKeypointsDataset, coco_results: COCOKeypointResults):
+
+    # create dict with categories and keypoint ids
+
+    distance_dict = {}
+    for category in coco_dataset.categories:
+        keypoint_dict = {}
+        for i, keypoint in enumerate(category.keypoints):
+            keypoint_dict[keypoint] = []
+        distance_dict[category.id] = keypoint_dict
+    
+    category_id_to_category = {category.id: category for category in coco_dataset.categories}
+
+    # first check if for each image, there is max one annotation
+    image_id_to_annotations = {}
+    for annotation in coco_dataset.annotations:
+        if annotation.image_id in image_id_to_annotations:
+            raise ValueError(f"Image {annotation.image_id} has multiple annotations.")
+        image_id_to_annotations[annotation.image_id] = annotation
+    
+    image_id_to_predictions = {}
+    for prediction in coco_results:
+        if prediction.image_id in image_id_to_predictions:
+            raise ValueError(f"Image {prediction.image_id} has multiple predictions.")
+        image_id_to_predictions[prediction.image_id] = prediction
+
+    # then for each annotation, find the prediction
+
+    for image_id, annotation in image_id_to_annotations.items():
+        if image_id not in image_id_to_predictions:
+            #TODO: take image center?
+            # for now pass
+            continue
+        prediction = image_id_to_predictions[image_id]
+
+        predicted_keypoints = prediction.keypoints
+        predicted_keypoints = [predicted_keypoints[i:i+3] for i in range(0, len(predicted_keypoints), 3)]
+        annotated_keypoints = annotation.keypoints
+        annotated_keypoints = [annotated_keypoints[i:i+3] for i in range(0, len(annotated_keypoints), 3)]
+
+        category_id = annotation.category_id
+        for i, name in enumerate(category_id_to_category[category_id].keypoints):
+            # calculate the distance between the predicted and annotated keypoint
+            # add the distance to the list
+            predicted_keypoint = predicted_keypoints[i]
+            annotated_keypoint = annotated_keypoints[i]
+
+            distance = ((predicted_keypoint[0] - annotated_keypoint[0])**2 + (predicted_keypoint[1] - annotated_keypoint[1])**2)**0.5
+            print(distance)
+            distance_dict[category_id][name].append(distance)
+        # calculate the distance for each keypoint, and add them to the list
+
+    # if there is no prediction, predict all keypoints to the center of the image.
+
+    # calculate the distance for each keypoint, and add them to the list 
+
+    
+    # if there are remaining predictions, for which there was no annotation, 
+    #TODO: what to to with these? cannot take FP into account in distance metric?.
+    # for now, just ignore them.
+
+    return distance_dict
+
+
+def calculate_average_distances(distance_dict):
+    # calculate the average distance for each keypoint
+    average_distance_dict = {}
+    for category_id, keypoint_dict in distance_dict.items():
+        average_distance_dict[category_id] = {}
+        for keypoint_id, distances in keypoint_dict.items():
+            average_distance_dict[category_id][keypoint_id] = sum(distances) / len(distances)
+    return average_distance_dict
+
+def calculate_median_distances(distance_dict):
+    # calculate the average distance for each keypoint
+    median_distance_dict = {}
+    for category_id, keypoint_dict in distance_dict.items():
+        median_distance_dict[category_id] = {}
+        for keypoint_id, distances in keypoint_dict.items():
+            median_distance_dict[category_id][keypoint_id] = sorted(distances)[len(distances)//2]
+    return median_distance_dict
+
+def calculate_std_deviation(distance_dict):
+    # calculate the average distance for each keypoint
+    std_deviation_dict = {}
+    for category_id, keypoint_dict in distance_dict.items():
+        std_deviation_dict[category_id] = {}
+        for keypoint_id, distances in keypoint_dict.items():
+            mean = sum(distances) / len(distances)
+            std_deviation_dict[category_id][keypoint_id] = (sum([(distance - mean)**2 for distance in distances]) / len(distances))**0.5
+    return std_deviation_dict
+
+if __name__ == "__main__":
+    dataset_path = "/home/tlips/Code/2D-keypoints-benchmark/test/data/dummy_keypoints.json"
+    coco_results_path ="/home/tlips/Code/2D-keypoints-benchmark/test/data/dummy_keypoint_results.json"
+    import json 
+    with open(dataset_path, "r") as f:
+        coco_dataset = CocoKeypointsDataset.parse_obj(json.load(f))
+    with open(coco_results_path, "r") as f:
+        coco_results = COCOKeypointResults.parse_obj(json.load(f))
+    distance_dict = calculate_keypoint_distances(coco_dataset, coco_results)
+    average_distance_dict = calculate_average_distances(distance_dict)
+    median_distance_dict = calculate_median_distances(distance_dict)
+    std_deviation_dict = calculate_std_deviation(distance_dict)
+    print(average_distance_dict)
+    # print(median_distance_dict)
+    # print(std_deviation_dict)
+
+
---
+++ kp_2d_benchmark/eval/coco_results.py
@@ -0,0 +1,250 @@
+"""parser for COCO dataset (both instance & keypoints) JSON files.
+
+The main reference for this parser can be found here:
+https://cocodataset.org/#format-data
+
+Dataset loading example:
+
+with open("path/to/annotations.json","r") as file:
+    data = json.load(file) # dict
+    parsed_data = CocoKeypoints(**data)
+
+When there are no keypoints, you may use CocoInstances instead of CocoKeypoints.
+
+
+Dataset creation example:
+
+coco_image = CocoImage(
+    id=1,
+    width=100,
+    height=100,
+    file_name="path/to/image.jpg",
+)
+images = [coco_image]
+
+# make more images, catergories and annotations here
+
+coco_keypoints = CocoKeypoints(categories=categories, images=images, annotations=annotations)
+with open("annotations.json", "w") as file:
+    json.dump(coco_keypoints.model_dump(exclude_none=True), file, indent=4)
+
+"""
+
+from typing import Dict, List, Optional, Sequence, Tuple, Union
+
+from pydantic import BaseModel, field_validator, model_validator,   RootModel
+
+# Used by CocoInfo and CocoImage
+Datetime = Union[
+    str, int
+]  # COCO uses both YYYY-MM-DD and YYYY-MM-DD HH:MM:SS for datetime, CVAT places 0 for missing values
+Url = str
+
+# Used by CocoImage and CocoLicenses
+LicenseID = int
+
+# Used by CocoCategory and CocoInstanceAnnotation
+CategoryID = int
+
+# Used by CocoImage and CocoInstanceAnnotation
+ImageID = int
+
+# Used by CocoInstanceAnnotation
+# Dict[int,int]  run length encoding(RLE) (of a pixel-mask), with keys "counts" and "size"
+# where count contains the actual run length encoding (of a pixel-mask) [x1,l1,x2,l2,...]
+# or a coco-encoded string that represents this list.
+# see https://www.youtube.com/watch?v=h6s61a_pqfM for a video explanation of RLE
+# see https://github.com/cocodataset/cocoapi/issues/386 for a discussion of RLE string encoding
+# RLE is Usually only used for is_crowd segmentations.
+RLEDict = Dict[str, Union[str, List]]
+
+Polygon = List[float]  # list of vertices [x1, y1, x2, y2, ...]
+Segmentation = Union[RLEDict, List[Polygon]]
+
+# Used by the Annotations
+Keypoints = List[float]  # list of keypoints [x1, y1, v1, x2, y2, v2, ...]
+IsCrowd = int  # 0 or 1
+
+
+class CocoInfo(BaseModel):
+    year: Union[int, str]
+    version: str
+    description: str
+    contributor: str
+    url: Url
+    date_created: Datetime
+
+
+class CocoImage(BaseModel):
+    id: ImageID
+    width: int
+    height: int
+    file_name: str  # relative path to image
+    license: Optional[LicenseID] = None
+    flicker_url: Optional[Url] = None
+    coco_url: Optional[Url] = None
+    date_captured: Optional[Datetime] = None
+    __hash__ = object.__hash__  # make hashable for use in set
+
+
+class CocoCategory(BaseModel, extra="allow"):
+
+    supercategory: str  # should be set to "name" for root category
+    id: CategoryID
+    name: str
+
+
+class CocoKeypointCategory(CocoCategory):
+    keypoints: List[str]
+    skeleton: Optional[List[List[int]]] = None
+
+
+class CocoInstanceAnnotation(BaseModel, extra="allow"):  # allow extra fields, to parse subclasses
+
+    id: int  # unique id for the annotation
+    image_id: ImageID
+    category_id: CategoryID
+
+    bbox: Optional[Tuple[float, float, float, float]] = None
+
+    segmentation: Optional[Segmentation] = None
+    area: Optional[float] = None
+    iscrowd: Optional[IsCrowd] = None
+
+    @field_validator("iscrowd")
+    @classmethod
+    def iscrowd_must_be_binary(cls, v: IsCrowd) -> IsCrowd:
+        if v is None:
+            return None
+        assert v in [0, 1]
+        return v
+
+class CocoKeypointAnnotation(CocoInstanceAnnotation):
+    keypoints: Keypoints
+    num_keypoints: Optional[int] = None
+
+    # make bbox optional
+    bbox: Optional[Tuple[float, float, float, float]] = None
+
+    @field_validator("keypoints")
+    @classmethod
+    def keypoints_must_be_multiple_of_three(cls, v: Keypoints) -> Keypoints:
+        assert len(v) % 3 == 0, "keypoints list must be a multiple of 3"
+        return v
+
+    @field_validator("keypoints")
+    @classmethod
+    def keypoints_coordinates_must_be_in_pixel_space(cls, v: Keypoints) -> Keypoints:
+        max_coordinate_value = 0.0
+        for i in range(0, len(v), 3):
+            max_coordinate_value = max(v[i], max_coordinate_value)
+            max_coordinate_value = max(v[i + 2], max_coordinate_value)
+        assert (
+            max_coordinate_value > 1
+        ), f"keypoints coordinates must be in pixel space, but max_coordinate is {max_coordinate_value}"
+        return v
+
+    @model_validator(mode="after")
+    def num_keypoints_matches_amount_of_labeled_keypoints(self) -> "CocoKeypointAnnotation":
+
+
+        labeled_keypoints = 0
+        for v in self.keypoints[2::3]:
+            if v > 0:
+                labeled_keypoints += 1
+
+        # if num_keypoints is not set, set it to the number of labeled keypoints
+        if self.num_keypoints is None:
+            self.num_keypoints = labeled_keypoints
+
+        assert (
+            labeled_keypoints == self.num_keypoints
+        ), f"num_keypoints {self.num_keypoints} does not match number of labeled of keypoints {labeled_keypoints} for annotation {self.id}"
+        return self
+
+
+class CocoLicense(BaseModel):
+    id: LicenseID
+    name: str
+    url: Url
+
+
+class CocoInstancesDataset(BaseModel):
+    info: Optional[CocoInfo] = None
+    licenses: Optional[List[CocoLicense]] = None
+    categories: Sequence[CocoCategory]
+    images: List[CocoImage]
+    annotations: Sequence[CocoInstanceAnnotation]
+
+    @field_validator("annotations")
+    @classmethod
+    def annotations_list_cannot_be_empty(cls, v: List[CocoInstanceAnnotation]) -> List[CocoInstanceAnnotation]:
+        assert len(v) > 0, "annotations list cannot be empty"
+        return v
+
+    # skip on failure becasue this validator requires the annotations list to be non-empty
+    @model_validator(mode="after")
+    def annotations_catergory_id_exist_in_categories(self) -> "CocoInstancesDataset":
+        category_ids = set([category.id for category in self.categories])
+        for annotation in self.annotations:
+            assert (
+                annotation.category_id in category_ids
+            ), f"Annotation {annotation.id} has category_id {annotation.category_id} which does not exist in categories."
+        return self
+
+
+class CocoKeypointsDataset(CocoInstancesDataset):
+    # Override the type of annotations.
+    # annotations must be Sequence vs. list to allow this, see:
+    # https://mypy.readthedocs.io/en/stable/common_issues.html#variance
+    categories: Sequence[CocoKeypointCategory]
+    annotations: Sequence[CocoKeypointAnnotation]
+
+    # skip on failure becasue this validator requires the annotations list to be non-empty
+    @model_validator(mode="after")
+    def num_keypoints_matches_annotations(self) -> "CocoKeypointsDataset":
+        category_dict = {category.id: category for category in self.categories}
+        for annotation in self.annotations:
+            assert len(annotation.keypoints) // 3 == len(
+                category_dict[annotation.category_id].keypoints
+            ), f"Number of keypoints for annotation {annotation.id} does not match number of keypoints in category."
+        return self
+
+
+
+class COCOKeypointResult(BaseModel):
+    image_id: int
+    category_id: int
+    keypoints: List[float]
+    score: float
+    per_keypoint_scores: Optional[List[float]] = None
+
+
+    @field_validator("keypoints")
+    @classmethod
+    def keypoints_must_be_multiple_of_three(cls, v: List[float]) -> List[float]:
+        assert len(v) % 3 == 0, "keypoints list must be a multiple of 3"
+        return v
+
+class COCOKeypointResults(RootModel):
+    root: List[COCOKeypointResult]
+
+    def __iter__(self):
+        return iter(self.root)
+
+    def __getitem__(self, item):
+        return self.root[item]
+
+    def __len__(self):
+        return len(self.root)
+
+if __name__ == "__main__":
+    path = "test/data/keypoint_results.json"
+    import json
+    with open(path, "r") as file:
+        data = json.load(file)
+        parsed_data = COCOKeypointResults(data)
+        print(parsed_data)
+
+        print(len(parsed_data))
+        print(parsed_data[0].image_id)
